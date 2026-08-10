53d5b76965c613ffabb0476272e479b2ba64cadd
bs7703 <bs770311@gmail.com>
2026-08-11 00:29:33 +0900

    로직: main.py: feat/io, feat/validator에서 검증과 입출력을 생성한것이 정상적으로 작동하는지 아주간단한 로직생성

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import srcs.core.validate
+import srcs.core.validate as val
+import srcs.core.utils_io as utils
+import srcs.specs.data_spec as spec
 
 def main():
-    pass
-
+    data = utils.load_json("data/data.json")
+    print(val.validate(data, spec.SCHEMA))
 if __name__ == "__main__":
     main()
